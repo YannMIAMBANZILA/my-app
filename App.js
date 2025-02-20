52dadd47aa6aa881418bf53a1e5289de9fa746e9
yann miambanzila <ymiambanzila@gmail.com>
2025-02-20 22:47:50 +0100

    second_commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Home from './screen/Home';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
---
+++ constants/api.js
@@ -0,0 +1,9 @@
+export const URL = {
+    FETCH_ARTICLES: 'http://localhost:8081/api/articles',
+    CREATE_ARTICLE: '',
+    UPDATE_ARTICLE: '',
+    DELETE_ARTICLE: '',
+    DRAFT : 'https://jsonplaceholder.typicode.com/posts',
+
+    
+}
